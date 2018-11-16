8f1b07cb63df9f38c8863fc52cc17ce704f918e9
rajkapadia28 <raj.kapadia28@gmail.com>
2018-11-16 15:47:27 -0500

    sign up page redirects to books. No authentication yet

--- src/routes/userLogRoute.js
+++ src/routes/userLogRoute.js
@@ -28,10 +28,13 @@ function router(){
                 );
         });
     
-    authRouter.post((req, res) =>{
+    // authRouter.post((req, res) =>{
+    //     res.redirect('/books');
+    // });
+
+    authRouter.post('/user/signup', function(req, res, next){
         res.redirect('/books');
     });
-
 
         return authRouter;
     }
