c6cc044647b9857de6f2a874342a041639eda471
Cesia Bulnes <cesiabulnes@gmail.com>
2018-11-20 19:51:09 -0500

    Isolate Books' query functionality

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -7,7 +7,7 @@ const host = 'cen4010dbinstance.cuo3jpom4wfm.us-east-1.rds.amazonaws.com';
 const database = 'cen4010db';
 const password = 'cen4010password';
 
-function router(nav) {
+function getBooksFromDb(sortBy, direction) {
   var client = new Client({
     user: user,
     host: host,
@@ -15,142 +15,85 @@ function router(nav) {
     password: password,
     port: 5432,
   });
-
   client.connect();
 
-  var ratingArray = [];
-  var ratingNumber = 0;
-  var books = [];
-  // Do your queries here
-  var result = client.query('SELECT book.book_id, book.book_title, book.book_description , book.book_price,author.author_name_first, g.genre_name, Count(r.review_rating), CAST(AVG(r.review_rating)AS DECIMAL(10,1)) FROM book JOIN book_author ba ON book.book_id=ba.book_id INNER JOIN author ON author.author_id=ba.author_id JOIN book_genre bg ON book.book_id=bg.book_id JOIN genre g ON bg.genre_id=g.genre_id LEFT Join review r ON book.book_id=r.book_id Group by book.book_id, book.book_title, author.author_name_first, g.genre_name',
-    (err, res) => {
-      for (i = 0; i < res.rows.length; i++) {
-        books.push(
-          {
-            id: res.rows[i].book_id,
-            title: res.rows[i].book_title,
-            genre: res.rows[i].genre_name,
-            author: res.rows[i].author_name_first,
-            ratingNumber: res.rows[i].count,
-            ratingAverage: res.rows[i].avg,
-            description: res.rows[i].book_description,
-            price: res.rows[i].book_price,
-            read: false
-          });
-        ratingArray.push(
-          {
-            ratingValue: ratingNumber,
-          });
+  return new Promise((resolve, reject) => {
+    let bookQuery = 'SELECT book.book_id, book.book_title, book.book_description , book.book_price, book.book_release_date,author.author_name_first, g.genre_name, Count(r.review_rating), CAST(AVG(r.review_rating)AS DECIMAL(10,1)) FROM book JOIN book_author ba ON book.book_id=ba.book_id INNER JOIN author ON author.author_id=ba.author_id JOIN book_genre bg ON book.book_id=bg.book_id JOIN genre g ON bg.genre_id=g.genre_id LEFT Join review r ON book.book_id=r.book_id Group by book.book_id, book.book_title, author.author_name_first, g.genre_name';
+    let realDirection = direction;
+    if (realDirection != 'desc' && realDirection != 'asc') {
+      realDirection = 'asc';
+    }
+    if (sortBy) {
+      if (!['book_title', 'book_id', 'author_name_first', 'book_price', 'book_release_date'].includes(sortBy)) {
+        sortBy = 'book_title'
       }
+      bookQuery = `SELECT * FROM (${bookQuery}) AS inner_query ORDER BY ${sortBy} ${realDirection}`;
+    }
 
-      client.end();
-    });
+    var books = [];
+    var result = client.query(
+      bookQuery,
+      (err, res) => {
+        if (err) {
+          reject(err);
+        }
 
-  // const books = [
-  //   {
-  //     id: 1,
-  //     title: 'War and Peace',
-  //     genre: 'Historical Fiction',
-  //     author: 'Lev Nikolayevich Tolstoy',
-  //     read: false
-  //   },
-  //   {
-  //     id: 2,
-  //     title: 'Les Misérables',
-  //     genre: 'Historical Fiction',
-  //     author: 'Victor Hugo',
-  //     read: false
-  //   },
-  //   {
-  //     id: 3,
-  //     title: 'The Time Machine',
-  //     genre: 'Science Fiction',
-  //     author: 'H. G. Wells',
-  //     read: false
-  //   },
-  //   {
-  //     id: 4,
-  //     title: 'A Journey into the Center of the Earth',
-  //     genre: 'Science Fiction',
-  //     author: 'Jules Verne',
-  //     read: false
-  //   },
-  //   {
-  //     id: 5,
-  //     title: 'The Dark World',
-  //     genre: 'Fantansy',
-  //     author: 'Henry Kuttner',
-  //     read: false
-  //   },
-  //   {
-  //     id: 6,
-  //     title: 'Thw Wind in the Willows',
-  //     genre: 'Fantasy',
-  //     author: 'Kenneth Grahame',
-  //     read: false
-  //   },
-  //   {
-  //     id: 7,
-  //     title: 'Life On The Mississippi',
-  //     genre: 'History',
-  //     author: 'Mark Twain',
-  //     read: false
-  //   },
-  //   {
-  //     id: 8,
-  //     title: 'Childhood',
-  //     genre: 'Biography',
-  //     author: 'Lev Nikolayevich',
-  //     read: false
-  //   }
-  // ];
+        for (i = 0; i < res.rows.length; i++) {
+          books.push(
+            {
+              id: res.rows[i].book_id,
+              title: res.rows[i].book_title,
+              genre: res.rows[i].genre_name,
+              author: res.rows[i].author_name_first,
+              ratingNumber: res.rows[i].count,
+              ratingAverage: res.rows[i].avg,
+              description: res.rows[i].book_description,
+              price: res.rows[i].book_price,
+              read: false
+            });
+        }
+        resolve(books);
 
+        client.end();
+      });
+  });
+}
+
+function router(nav) {
   bookRouter.route('/')
-    .get((req, res) => {
-      (async function query() {
-        //const request = new sql.Request();
-        // const { recordset } = await request.query('SELECT * FROM books');
-        // debug(result);
+    .get(async (req, res) => {
+        const sortBy = req.query.sort_by;
+        const direction = req.query.direction;
         res.render(
           'bookListView',
           {
             nav,
             title: 'Library',
+            books: await getBooksFromDb(sortBy, direction)
+          }
+        );
+    });
+
+  bookRouter.route('/:id')
+    .get(async (req, res) => {
+        const { id } = req.params;
+        const books = await getBooksFromDb();
+        // [{id, title, genre, author, ...}]
+        const specificBook = books.filter(({book_id}) => {
+          return book_id == id;
+        });
+
+        res.render(
+          'bookView',
+          {
+            nav,
+            title: 'Library',
+            book: specificBook,
             books: books
           }
         );
-      }());
     });
 
-  /*
-  .all((req, res, next) => {
-      (async function query() {
-        const { id } = req.params;
-        const request = new sql.Request();
-        const { recordset } =
-          await request.input('id', sql.Int, id)
-            .query('SELECT * FROM books WHERE id = @id');
-        [req.book] = recordset;
-        next();
-
-      }());
-    })
-  */
-
-  bookRouter.route('/:id')
-    .get((req, res) => {
-      const { id } = req.params;
-      const specificBook = books[id - 1];
-      res.render(
-        'bookView',
-        {
-          nav,
-          title: 'Library',
-          book: specificBook,
-          books: books
-        }
-      );
-    });
   return bookRouter;
 }
 
