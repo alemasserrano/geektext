615570549f0a5291cc65d9cd5992fcbdb7bb10a2
alemasserrano <mmass004@fiu.edu>
2018-11-22 02:17:57 -0500

    added author and publisher info to detailspage

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -21,7 +21,7 @@ function router(nav) {
   var ratingNumber = 0;
   var books = [];
   // Do your queries here
-  var result = client.query('SELECT book.book_id, book.book_title, book.book_description , book.book_price, author.author_name_first, book.book_image, g.genre_name, Count(r.review_rating), CAST(AVG(r.review_rating)AS DECIMAL(10,1)) FROM book JOIN book_author ba ON book.book_id=ba.book_id INNER JOIN author ON author.author_id=ba.author_id JOIN book_genre bg ON book.book_id=bg.book_id JOIN genre g ON bg.genre_id=g.genre_id LEFT Join review r ON book.book_id=r.book_id Group by book.book_id, book.book_title, author.author_name_first, g.genre_name',
+  var result = client.query('SELECT book.book_id, book.book_title, book.book_description , book.book_price, author.author_name_first, author.author_name_last, author.author_biography, author.author_id, book.book_image, g.genre_name, p.publisher_name, book.book_release_date, Count(r.review_rating), CAST(AVG(r.review_rating)AS DECIMAL(10,1)) FROM book JOIN book_author ba ON book.book_id=ba.book_id INNER JOIN author ON author.author_id=ba.author_id JOIN book_genre bg ON book.book_id=bg.book_id JOIN genre g ON bg.genre_id=g.genre_id LEFT Join review r ON book.book_id=r.book_id JOIN publisher p ON book.publisher_id=p.publisher_id Group by book.book_id, book.book_title, author.author_name_first, author.author_name_last, author.author_biography, author.author_id, g.genre_name, p.publisher_name, book.book_release_date',
     (err, res) => {
       for (i = 0; i < res.rows.length; i++) {
         books.push(
@@ -30,11 +30,15 @@ function router(nav) {
             img: res.rows[i].book_image,
             title: res.rows[i].book_title,
             genre: res.rows[i].genre_name,
-            author: res.rows[i].author_name_first,
+            author_id: res.rows[i].author_id,
+            author_name: res.rows[i].author_name_first + " " + res.rows[i].author_name_last,
+            author_bio: res.rows[i].author_biography,
             ratingNumber: res.rows[i].count,
             ratingAverage: res.rows[i].avg,
             description: res.rows[i].book_description,
             price: res.rows[i].book_price,
+            publisher_name: res.rows[i].publisher_name,
+            release_date: res.rows[i].book_release_date,
             read: false
           });
         ratingArray.push(
