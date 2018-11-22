9830def89a48048759e2287c9d3d2c25a65642e7
rajkapadia28 <raj.kapadia28@gmail.com>
2018-11-22 10:34:53 -0500

    updated app.js to work with user post. Removed unecessary dependencies.

--- app.js
+++ app.js
@@ -7,45 +7,67 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const flash = require('connect-flash');
+const pgStore = require('connect-pg-simple')(session);
+const { Pool, Client } = require('pg');
+const p = require('./queries');
+var expressSession = require('express-session');
+
+
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-const config = {
-  user: 'library',
-  password: 'IG@MSAzur3',
-  server: 'igpslibrary.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-  database: 'PSLibrary',
-  options: {
-    encrypt: true // Use this if you're on Windows Azure
-  }
-};
-
-// sql.connect(config).catch(err => debug(err));
+//var pgSession = require('connect-pg-simple')(session);
 
 app.use(morgan('tiny'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// sessionStorage = new pgSession({
+//   conString: config.db.connection,
+//   tableName: config.session.tableName
+// });
 app.use(session({ 
-  secret: 'library',
-  resave: true,
-  saveUninitialized: true
+  secret: 'keyboard cat',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {maxAge: 180 * 60 * 1000 }
+  // store: new pgStore(p.db)
 }));
-
-require('./src/config/passport.js')(app);
-
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(expressSession({secret: 'keyboard cat'}))
+app.use(bodyParser());
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+// require('./src/config/passport.js')(app);
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use((req, res, next) => {
   debug('my middleware');
   next();
 });
+
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
+
+
+
+app.use(function(req, res, next){
+   res.locals.login = req.isAuthenticated();
+    res.locals.session = req.session;
+    delete req.session.user;
+
+    next();
+})
 
 const nav = [
   { link: '/books', title: 'Book' },
@@ -56,24 +78,26 @@ const nav = [
 const bookRouter = require('./src/routes/bookRoutes')(nav);
 // const adminRouter = require('./src/routes/adminRoutes')(nav);
 // const authRouter = require('./src/routes/authRoutes')(nav);
-const authRouter = require('./src/routes/userLogRoute')();
+const authRouter = require('./src/routes/userAuth')(app);
 const reviewRouter = require('./src/routes/review')(nav);
 const cartRouter = require('./src/routes/cartRoute')(nav);
 
 app.use('/books', bookRouter);
 // app.use('/admin', adminRouter);
-app.use('/', authRouter);
+app.use('/user/signin', authRouter);
 app.use('/review', reviewRouter);
 app.use('/cart', cartRouter);
 
 app.get('/', (req, res) => {
   res.render(
-    'index',
+    'user/signin',
     {
       nav: [{ link: '/books', title: 'Books' },
       { link: '/authors', title: 'Authors' },
         { link: '/cart', title: 'Cart'}],
-      title: 'Library'
+      title: 'Library',
+      messages: {danger: req.flash('danger'), warning: req.flash('warning'), success: req.flash('success')}
+
     }
   );
 });
